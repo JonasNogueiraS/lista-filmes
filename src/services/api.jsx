7aa5dee8b9cdc0e65d2810ff5925d42322c8c29e
JonasNogueiraS <jonasnogueiira@gmail.com>
2025-08-15 16:26:09 -0300

    Correção de API key

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,4 @@
 import axios from 'axios'
-// BASE da URL: https://api.themoviedb.org/3
-// URL API: /movie/now_playing?api_key=2036df264b59354ad360c8e877a0294f&language=pt-BR
 
 const api = axios.create({
     baseURL: 'https://api.themoviedb.org/3'
